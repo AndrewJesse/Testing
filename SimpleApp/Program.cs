c329f4e9817c8e8b5b0e322f0886a536eed735f6
A.J. Marchese <aj.marchese@outlook.com>
2023-03-06 08:16:27 -0800

    Added XUnit testing class

--- SimpleApp/Program.cs
+++ SimpleApp/Program.cs
@@ -1,6 +1,14 @@
+// Creates a new instance of the web application builder
 var builder = WebApplication.CreateBuilder(args);
+
+// Registers controllers with views as services in the DI container
+builder.Services.AddControllersWithViews();
+
+// Builds the application with the registered services
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Maps default controller route for the application
+app.MapDefaultControllerRoute();
 
+// Runs the application
 app.Run();
